7545e919ea9df6f56cdda5ceacd03f5900a6d489
Lucas Almeida <lucasaclima03@gmail.com>
2021-08-08 16:53:59 -0300

    estrutura inicial do projeto

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+
+// ------- DATABASE -------
+const db = require('./models/index')
+
+db.sequelize.sync()
+
+// - WARNING!!!!! Lines above drop the database and start over
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log("Drop and re-sync db.")
+// })
+
+
+// ------- IMPORTING ROUTES -------
+const routeUsers = require('./routes/admin-router')
+const routeClients = require('./routes/teachers-routes')
+const routeClients = require('./routes/students-routes')
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({extend: false}))
+app.use(express.json())
+app.use(cors())
+
+// ------- REGISTERING ROUTES -------
+app.use('/admin', routeAdmin)
+app.use('/teachers', routeTeachers)
+app.use('/students', routeStudents)
+
+app.use((req, res, next) => {
+    return res.status(200).send({
+        mensagem: "Rota não encontrada"
+    })
+})
+
+module.exports = app
